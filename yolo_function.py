1c38304dd7aaee493dc666c10ac865bcd98731c5
Marco Schott <marcoschott@outlook.com>
2023-05-06 17:26:25 +0200

    Rename yolo directory + direct call

--- yolo_function.py
+++ yolo_function.py
@@ -1,13 +1,21 @@
 import pickle
 import os
+import cv2
 
-def run_yolo(img_path):
-  os.system(f'! python yolov5-master/detect.py --weights yolov5n.pt --img 640 --conf 0.25 --source {img_path}')
-  with open('saved_dictionary.pkl', 'rb') as f:
-    loaded_dict = pickle.load(f)
+import yolov5master.detect as det
 
-  new_dict_with_centers = []
-  for element in loaded_dict:
-    center = [element[1][0] + element[1][2] // 2, element[1][1] + element[1][3] // 2]
-    new_dict_with_centers.append([element[0], center])
-  return new_dict_with_centers
+
+def run_yolo(img):
+    det.run(weights='yolov5n.pt', source=img)
+    with open('saved_dictionary.pkl', 'rb') as f:
+        loaded_dict = pickle.load(f)
+
+    new_dict_with_centers = []
+    for element in loaded_dict:
+        center = [element[1][0] + element[1][2] //
+                  2, element[1][1] + element[1][3] // 2]
+        new_dict_with_centers.append([element[0], center])
+    return new_dict_with_centers
+
+
+print(run_yolo("newtest.jpg"))
