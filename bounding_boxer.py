b1ec9df288e3cac91eab338feee7fcdcab24c962
Marco Schott <marcoschott@outlook.com>
2023-05-07 00:30:25 +0200

    Fix boundingbox methods

--- bounding_boxer.py
+++ bounding_boxer.py
@@ -15,36 +15,29 @@ tracked = ["bottle", "apple", "carrot"]
 def getHandBB(image):
     
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    hands = handDetector.getHandBoxes(imageRGB)
+    
+    for (minx, miny, maxx, maxy) in hands:
+        cv2.circle(image, ((minx + maxx) // 2, ((miny + maxy) // 2)), 25, (255, 0, 255), cv2.FILLED)
 
-
-    fingers = handDetector.getIndexLocation(imageRGB)
-    #print(boxes)
-    for (cx, cy) in fingers:
-        cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-
-    boundingBox = fingers # TODO Fix to BB
-
-    return boundingBox, image
+    return hands, image
     
     
 def getObjectBB(image):
     
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    
     boxes = objectDetector.getObjectBoxes(imageRGB)
-    
+
+    results = []
+
     for box in boxes.iterrows():
         box = box[1]
         #print(box)
         if box["name"] in tracked:
             cx = int((box["xmin"] + box["xmax"]) / 2)
             cy = int((box["ymin"] + box["ymax"]) / 2)
+
+            results.append([box["xmin"], box["ymin"], box["xmax"], box["ymax"]])
             cv2.circle(image, (cx, cy),25, (255, 0, 0), cv2.FILLED)
             
-    boundingBox = boxes # TODO fix Marco!!!!
-            
-    return boundingBox, image
-            
-
-    
+    return results, image
--- detection/hand.py
+++ detection/hand.py
@@ -4,19 +4,27 @@ import mediapipe as mp
 class HandDetector:
     hands = mp.solutions.hands.Hands()
 
-    def getIndexLocation(self, image):
+    def getHandBoxes(self, image): 
         results = self.hands.process(image)
 
-        fingers = []
+        handBoxes = []
 
         # checking whether a hand is detected
         if results.multi_hand_landmarks:
+            minx, miny = image.shape
+            maxx = 0
+            maxy = 0
             for handLms in results.multi_hand_landmarks:  # working with each hand
                 for id, lm in enumerate(handLms.landmark):
                     h, w, c = image.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
 
-                    if id == 8:
-                        fingers.append((cx, cy))
+                    minx = min(cx, minx)
+                    miny = min(cy, miny)
+                    maxx = max(cx, maxx)
+                    maxy = max(cy, maxy)
 
-        return fingers
+                    if minx <= maxx and miny <= maxy :
+                        handBoxes.append([minx, miny, maxx, maxy])
+
+        return handBoxes
