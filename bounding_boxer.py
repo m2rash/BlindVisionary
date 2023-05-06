81cecb0db5d6286269c67722a8780010c11b1a46
Lukas Schölch <Lukas.schoelch@gmail.com>
2023-05-07 00:09:20 +0200

    bounding_boxer created

--- bounding_boxer.py
+++ bounding_boxer.py
@@ -11,18 +11,29 @@ objectDetector = do.ObjectDetector()
 
 tracked = ["bottle", "apple", "carrot"]
 
-while True:
-    success, image = cap.read()
+
+def getHandBB(image):
+    
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    #gray = cv2.resize(imageRGB, [imageRGB.shape[0] // 2, imageRGB.shape[1] // 2], interpolation = cv2.INTER_AREA)
 
     fingers = handDetector.getIndexLocation(imageRGB)
-    boxes = objectDetector.getObjectBoxes(imageRGB)
     #print(boxes)
     for (cx, cy) in fingers:
         cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
+    boundingBox = fingers # TODO Fix to BB
+
+    return boundingBox, image
+    
+    
+def getObjectBB(image):
+    
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    
+    boxes = objectDetector.getObjectBoxes(imageRGB)
+    
     for box in boxes.iterrows():
         box = box[1]
         #print(box)
@@ -30,7 +41,10 @@ while True:
             cx = int((box["xmin"] + box["xmax"]) / 2)
             cy = int((box["ymin"] + box["ymax"]) / 2)
             cv2.circle(image, (cx, cy),25, (255, 0, 0), cv2.FILLED)
-        #print(box.name)
+            
+    boundingBox = boxes # TODO fix Marco!!!!
+            
+    return boundingBox, image
+            
 
-    cv2.imshow("Output", image)
-    cv2.waitKey(1)
+    
